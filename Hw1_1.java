6dea4834ca254f71663f197b4d4ab003772e3a4b
DiNikOs <demon22odn08@rambler.ru>
2019-01-17 08:26:28 +0300

    /

--- Hw1_1.java
+++ Hw1_1.java
@@ -1,4 +1,4 @@
-п»ї/*
+/*
  *Java, hw1
  * @author Dmitriy Ostrovskiy
  * @version 1.2
@@ -22,42 +22,42 @@ public class Hw1_1 {
         System.out.println("float f = " + f);
         System.out.println("double d = " + g);
         System.out.println("boolean a = " + n);
-        System.out.println("char c = " + h);                                    //Р”Р—_1.2
-        System.out.println("a*(b+(cd))= " + calculate(1,2,3,4));     		//Р”Р—_1.3
-        System.out.println("10<=(one+two)>=20 = " + task10and20(7,10)); 	//Р”Р—_1.4
-        isPositiveOrNegative(7);                                            	//Р”Р— 1.5
-        System.out.println("Р§РёСЃР»Рѕ РѕС‚СЂРёС†Р°С‚РµР»СЊРЅРѕРµ " + isNegative(-7));       	//Р”Р— 1.6
-        greetings ("РЎРµСЂРіРµР№");                                             	//Р”Р— 1.7
-        getYear(2000);                                                          //Р”Р— 1.8
+        System.out.println("char c = " + h);                                    //ДЗ_1.2
+        System.out.println("a*(b+(c/d))= " + calculate(1,2,3,4));     		//ДЗ_1.3
+        System.out.println("10<=(one+two)>=20 = " + task10and20(7,10)); 	//ДЗ_1.4
+        isPositiveOrNegative(7);                                            	//ДЗ 1.5
+        System.out.println("Число отрицательное " + isNegative(-7));       	//ДЗ 1.6
+        greetings ("Сергей");                                             	//ДЗ 1.7
+        getYear(2000);                                                          //ДЗ 1.8
 
     }
-    public static float calculate(float a,float b, float c, float d){           //Р”Р—_1.3
+    public static float calculate(float a,float b, float c, float d){           //ДЗ_1.3
           float result= 0;
-            if (d==0) System.out.println("Р§РёСЃР»Рѕ d РЅРµ РґРѕР»Р¶РЅРѕ Р±С‹С‚СЊ = 0");
+            if (d==0) System.out.println("Число d не должно быть = 0");
             else result=a*(b+(c/d));
        return  result;
     }
-    public static boolean task10and20(int one,int two){                          //Р”Р—_1.4
+    public static boolean task10and20(int one,int two){                          //ДЗ_1.4
         int sym= one + two;
         if (sym<=20&&sym>=10)return true;
         else return false;
     }
-    public static void isPositiveOrNegative(int num){                            //Р”Р— 1.5
-        if (num>=0) System.out.println(num +  " РџРѕР»РѕР¶РёС‚РµР»СЊРЅРѕРµ С‡РёСЃР»Рѕ");
-        else System.out.println(num +  " РћС‚СЂРёС†Р°С‚РµР»СЊРЅРѕРµ С‡РёСЃР»Рѕ");
+    public static void isPositiveOrNegative(int num){                            //ДЗ 1.5
+        if (num>=0) System.out.println(num +  " Положительное число");
+        else System.out.println(num +  " Отрицательное число");
     }
-    public static boolean isNegative(int num){                                   //Р”Р— 1.6
+    public static boolean isNegative(int num){                                   //ДЗ 1.6
         if (num<=0) return true ;
         else return false;
     }
-    public static void greetings(String name){                                   //Р”Р— 1.7
-        System.out.println("РџСЂРёРІРµС‚, "  + name);
+    public static void greetings(String name){                                   //ДЗ 1.7
+        System.out.println("Привет, "  + name);
     }
-    public static void getYear(int year){                                        //Р”Р— 1.8
+    public static void getYear(int year){                                        //ДЗ 1.8
         if (year%4!=0||(year%400!=0&&year%100==0))  {
-            System.out.println(year +  " РіРѕРґ РЅРµ РІРёСЃРѕРєРѕСЃРЅС‹Р№" );
+            System.out.println(year +  " год не високосный" );
             return;
         }
-        else if (year%4==0) System.out.println(year +  " РіРѕРґ РІРёСЃРѕРєРѕСЃРЅС‹Р№" );
+        else if (year%4==0) System.out.println(year +  " год високосный" );
     }
 }
